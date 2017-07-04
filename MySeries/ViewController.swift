fd4898e4f86bce49d0a862161565865a1563c1cf
Lucas Santos <lucas.santos@wooba.com.br>
2017-07-04 09:01:40 -0300

    Initial Commit

---
+++ MySeries/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySeries
+//
+//  Created by Dev on 04/07/17.
+//  Copyright © 2017 Wooba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
